211e93d032cb393e98b773471a8a2de965b70340
joacripp <joaquincrippa@gmail.com>
2022-05-31 15:04:06 -0300

    Implement Trainer

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='TaxiFareModel',
-      version="1.0",
+setup(name='TaxiFareModel_joacripp',
+      version="1.1",
       description="Project Description",
       packages=find_packages(),
       install_requires=requirements,
